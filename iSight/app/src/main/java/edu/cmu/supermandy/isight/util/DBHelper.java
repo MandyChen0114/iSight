288e7dec58cdcf64119daf1c456193fda661d75d
arbiter527 <arbiter527@gmail.com>
2016-04-30 23:13:09 -0700

    add db, fix sign up page: age

--- iSight/app/src/main/java/edu/cmu/supermandy/isight/util/DBHelper.java
+++ iSight/app/src/main/java/edu/cmu/supermandy/isight/util/DBHelper.java
@@ -11,7 +11,8 @@ public class DBHelper extends SQLiteOpenHelper {
     private final static String DB_NAME = "iSight.db";
     private final static String User_Table = "UserTable";
     private final static String Quiz_Table = "QuizTable";
-    private final static int VERSION = 2;
+        private final static String Record_Table = "RecordTable";
+    private final static int VERSION = 3;
 
     /**
      * constructors
@@ -27,8 +28,8 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create user info table
-        StringBuilder sql = new StringBuilder();
-        sql.append("create table if not exists UserTable(")
+        StringBuilder sql_user = new StringBuilder();
+        sql_user.append("create table if not exists UserTable(")
                 .append("Id integer primary key autoincrement,")
                 .append("Username text,")
                 .append("Email text,")
@@ -41,20 +42,33 @@ public class DBHelper extends SQLiteOpenHelper {
                 .append("Id integer primary key autoincrement,")
                 .append("Question text,")
                 .append("Answer text);");
+        StringBuilder sql_record = new StringBuilder();
+        // Create record info table
+        sql_record.append("create table if not exists RecordTable(")
+                .append("Id integer primary key autoincrement,")
+                .append("UserId integer,")
+                .append("TestId integer,")
+                .append("Timestamp date,")
+                .append("result text,")
+                .append("FOREIGN KEY(UserId) REFERENCES UserTable(Id) on delete cascade on update cascade);");
         System.out.println("---------------------->success1");
-        db.execSQL(sql.toString());
+        db.execSQL(sql_user.toString());
         System.out.println("---------------------->success2");
         db.execSQL(sql_quiz.toString());
         System.out.println("---------------------->success3");
+        db.execSQL(sql_record.toString());
+        System.out.println("---------------------->success4");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + User_Table);
-        System.out.println("---------------------->success4");
-        db.execSQL("DROP TABLE IF EXISTS " + Quiz_Table);
         System.out.println("---------------------->success5");
-        onCreate(db);
+        db.execSQL("DROP TABLE IF EXISTS " + Quiz_Table);
         System.out.println("---------------------->success6");
+        db.execSQL("DROP TABLE IF EXISTS " + Record_Table);
+        System.out.println("---------------------->success7");
+        onCreate(db);
+        System.out.println("---------------------->success8");
     }
 }
